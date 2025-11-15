0639eb57b63dd820d9037d6a23ac5d74ebe65649
sravanthigantasala96-del <sravanthigantasala.96@gmail.com>
2025-11-15 20:58:11 +0100

    Added TotalMoves and BestScore Functionality

--- src/components/ScoreBoard.jsx
+++ src/components/ScoreBoard.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
+import React from "react";
 
-function ScoreBoard() {
+function ScoreBoard(props) {
+  const { totalMoves, bestScore } = props;
   return (
-    <div className='score-info'>
-      
-        <p>Moves <span>7</span></p>
-        <p>Time  3:05</p>
-        <p>Best score  5</p>
-
+    <div className="score-info">
+      <p>
+        Moves <span>{totalMoves}</span>
+      </p>
+      <p>Time 3:05</p>
+      <p>Best score :{bestScore == Infinity ? "∞" : bestScore}</p>
     </div>
-  )
+  );
 }
 
-export default ScoreBoard
+export default ScoreBoard;
